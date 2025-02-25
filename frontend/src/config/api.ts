f09e15a692f478f0d196169efe1bd67b0d14dbe6
axiomproject <johncharlesvergara01@gmail.com>
2025-02-26 02:45:40 +0800

    xcx

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://kkmk-test-ncal.onrender.com/';
+const apiUrl = 'https://kkmk-test-ncal.onrender.com';
 
 const api = axios.create({
   baseURL: apiUrl,
